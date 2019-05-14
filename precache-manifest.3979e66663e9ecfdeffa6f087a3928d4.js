0008dc3190aeddeb0b8a96930fd1ec30f35ff3c5
John Holler <jh43051@gmail.com>
2019-05-14 16:24:40 -0400

    Add files via upload

---
+++ precache-manifest.3979e66663e9ecfdeffa6f087a3928d4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1400833bd56849accadc3c8f69eb947a",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/pomodoro-clock/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "70761ea6354c8aae3d66",
+    "url": "/pomodoro-clock/static/js/main.bb9ea3ea.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  }
+]);
